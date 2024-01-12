8d28d85addb65336f3a5a97d6e8270b2a642260d
charlesdowdell <chasedowdell@gmail.com>
2024-01-11 19:26:01 -0500

    Refactor

--- utils/configurations.py
+++ utils/configurations.py
@@ -1,16 +1,15 @@
 import yaml
 
 
-def _load_config(config_file):
-    with open(config_file, 'r') as file:
-        config = yaml.safe_load(file)
-    return config
-
-
 class Configurations:
 
-    def __init__(self, config_file='/Users/charlesdowdell/PycharmProjects/sprintswarm/config.yml'):
-        self._config = _load_config(config_file)
+    def __init__(self, config_file='../config.yml'):
+        self._config = self._load_config(config_file)
+
+    def _load_config(self, config_file):
+        with open(config_file, 'r') as file:
+            config = yaml.safe_load(file)
+        return config
 
     def get(self, *keys, default=None):
         config = self._config
